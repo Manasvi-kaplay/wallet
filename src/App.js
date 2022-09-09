241ec09e2d3f0f66bb2a2ec93ed4b2d0cc16e7c1
Manasvi Kaplay <manasvi111.kaplay@gmail.com>
2022-09-09 18:35:47 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './components/shared/Nav';
+import Welcome from './components/Welcome';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import dashboard from './components/dashboard/dashboard';
+import CreateWallet from './components/dashboard/dashboardOperations/CreateWallet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+        <Route path="/" component={Nav} /> 
+       <Routes>
+          <Route path="/" exact component={Welcome} />
+         <Route path="/dashboard" exact component={dashboard} />
+         <Route path="/CreateWallet" exact component={CreateWallet} /> 
+       </Routes>
+      <h1>Hello</h1>
+     </BrowserRouter>
+    //<h1>Hello There</h1>
     </div>
   );
 }
